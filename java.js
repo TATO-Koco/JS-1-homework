0f9cffac80bfcf7dbf0be9a105025c7b142cabb8
Tato <tatijavakhishvili@gmail.com>
2022-07-22 20:43:35 +0400

    Create java.js

---
+++ java.js
@@ -0,0 +1,78 @@
+// 1.
+
+
+// let  user = {
+//     firstname:'giorgi',
+//     lastname: 'smith',
+//     age: 25,
+//     studentstatus: 'active'
+// };
+// console.log(user.studentstatus);    
+
+// 2.
+
+
+// var number = [56,77,24,23]
+
+// console.log(number);
+
+
+// 3.
+
+// var number = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+
+// for(var i = 0; i<number.length; i++){
+//     if(number[i]>5){
+//         console.log(number[i]);
+//     }
+// }
+
+
+// 4.
+
+// var user = {
+// 	name: 'Giorgi',
+// 	age:  20,
+// 	studentstatus: 'active',
+// };
+
+// if (user.age <18 && user.studentstatus == 'active'){
+//     console.log('hello');
+// }else if (user.name = 'Levani'){
+//     console.log('Hello Giorgi');
+// }else if (user.studentstatus == 'active' || user.age < 25){
+//     console.log('hello world');
+// }else {
+//     console.log('error');
+// }
+
+//აქ რატომღაც ჰელუ გიორგის მიგდებს დანარჩენები გავტერსტე ყვალფერი ცალცალკე და სწორი იყო და მერე ყველა ერთად რომ გავუშვუიო მაინც ჰელუ გიორგის მიწერს და ავერ გავიგე რატო
+
+// 5.
+
+// var array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas' ];
+
+// console.log(array.typeof);
+
+//ვერც ეს ვერ გავიგე რამიე მინიშნება თუ შეიძლება )
+
+// 6.
+
+//ვერც ეს ვერ გავიგე დაახლოებით მეხუთესნაირია რა..
+
+// 7.
+
+// var array = [ [2, -3, 5, 10], [25, -24, -11, 100] , [-6, -7, 10] ]
+
+// for (let i = 0; i < array.length; i++) {
+//     const numbers = array[i];
+
+// for (let x = 0; x < numbers.length; x++) {
+//     const rational = numbers[x];
+//     if (rational > 0)
+//     console.log(rational);
+// }
+// }
+
+
+//დამატებითისთვის დრო აღარ მაქვს
